c100f39fb0bec30ab1cc23d41fd8774cc6defb6d
zihuiliu <zihuiliu@umich.edu>
2024-09-20 14:15:46 -0400

    refactor

--- protoc.sh
+++ protoc.sh
@@ -15,6 +15,7 @@ go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 protoc --go_out=./golang --go_opt=paths=source_relative \
   --go-grpc_out=./golang --go-grpc_opt=paths=source_relative \
  ./${SERVICE_NAME}/*.proto
+mkdir -p golang
 cd golang/${SERVICE_NAME}
 go mod init \
   github.com/OkabeRintarouBeta/microservice-protobuf/golang/${SERVICE_NAME} || true
